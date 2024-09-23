8283527df6d2cccb85548c3c081f6f9eb831ba87
Favvie <favourabangwu@gmail.com>
2024-09-23 19:24:32 +0100

    fix: remove redundant code

--- contracts/MerkleAirdrop.sol
+++ contracts/MerkleAirdrop.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-
+// import "./interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -1,10 +1,11 @@
 import { ethers } from "hardhat";
 
+
 async function main() {
     const ERC20MockAddress = "0x1769D9B9725C71EAF34Bc77b928E478C08792Bfc";
     const ERC20Mock = await ethers.getContractAt("IERC20", ERC20MockAddress);
 
-    const MerkleAirdropContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const MerkleAirdropContractAddress = "0x36C21ad4b2088a8Db6C121A77D7D8d74f3E3FD13";
     const MerkleAirdrop = await ethers.getContractAt("IMerkleAirdrop", MerkleAirdropContractAddress);
 
     // Approve savings contract to spend token
@@ -12,6 +13,7 @@ async function main() {
 
     const approveTx = await ERC20Mock.approve(MerkleAirdrop, approvalAmount);
     approveTx.wait();
+    // console.log(approveTx)
 
     // Specify the address that is eligible to claim (from the proof)
     const claimerAddress = "0x21Be2291f91EA2A1d1EB65DbBea2dA8886Ad7a3E";  // Replace with the actual address
@@ -34,12 +36,14 @@ console.log("Balance before airdrop:", ethers.formatUnits(balanceBefore, 18));
         "0x4d0b524ba70abe8e206fc34f9929bb6bfe978f1d275f766d5eb9510e6b0c0fd7",
         "0xc30c86e259aa2ef18502fae0e09559e01ea3b5d1f56c00eab5a85a4fa9ffa408"
       ]
+    //   const bytes32Proof = proof.map(p => ethers.hexlify(p));
     // const depositTx = await saveERC20.deposit(depositAmount);
+    // console.log(bytes32Proof)
     const claimAirdrop = await MerkleAirdrop.connect(claimerSigner).claimAirdrop(claimAmount, proof);
 
-    // console.log(depositTx);
+    console.log("claimAirdrop",claimAirdrop);
 
-    // depositTx.wait();
+    // // depositTx.wait();
 
     claimAirdrop.wait();
 
@@ -47,16 +51,17 @@ console.log("Balance before airdrop:", ethers.formatUnits(balanceBefore, 18));
 
     // console.log("Contract balance after :::", contractBalanceAfterDeposit);
     const balanceAfter = await ERC20Mock.balanceOf(claimerAddress);
-    console.log("Balance after airdrop:", ethers.formatUnits(balanceAfter, 18));
+    console.log("Balance after airdrop:", ethers.formatUnits(balanceAfter
+        , 18));
 
-    // Verify if the balance increased by the claim amount
-    const balanceChange = balanceAfter - balanceBefore;
-    if (balanceChange === claimAmount) {
-        console.log("Airdrop successfully received:", ethers.formatUnits(claimAmount, 18), "tokens");
-    } else {
-        console.log("Airdrop claim failed or incorrect amount received.");
-    }
-    console.log('airdrop claimed')
+    // // Verify if the balance increased by the claim amount
+    // // const balanceChange = balanceAfter - balanceBefore;
+    // if (balanceChange === claimAmount) {
+    //     console.log("Airdrop successfully received:", ethers.formatUnits(claimAmount, 18), "tokens");
+    // } else {
+    //     console.log("Airdrop claim failed or incorrect amount received.");
+    // }
+    // console.log('airdrop claimed')
 
 
 
